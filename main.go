d2dbbb0d1fccaeb2bbea07eb02b15097da2d5ec1
Leopold-pieto <82439276+Leopold-pieto@users.noreply.github.com>
2021-04-13 17:28:37 +0200

    Update main.go

--- main.go
+++ main.go
@@ -6,9 +6,9 @@ import (
 	"net/http"
 )
 
-const https = "https://"
-const path = "euw1.api.riotgames.com/lol/platform/v3/champion-rotations?"
-const APIkey = "api_key=RGAPI-d2a846d6-a608-453a-9c5d-3ecbc0a1c3d1" //valable 24h
+const https = "https://euw1.api.riotgames.com"
+const path = "/lol/summoner/v1/summoners/by-name/mouton211"
+const APIkey = "?api_key=RGAPI-d2a846d6-a608-453a-9c5d-3ecbc0a1c3d1" //valable 24h
 
 func api(url string) {
 	response, err := http.Get(url)
@@ -20,5 +20,8 @@ func api(url string) {
 
 func main() {
 	url := https + path + APIkey
+	fmt.Println(url)
+	fmt.Println()
+
 	api(url)
 }
