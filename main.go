2ba522ab10e24d94946cb36dd8228a519709eadf
Leopold-pieto <82439276+Leopold-pieto@users.noreply.github.com>
2021-04-13 11:39:56 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,18 @@
-
 package main
 
 import (
-    "fmt"
-    "net/http"
-    
+      "net/http"
+
 )
 
-func main() {
-    resp, err := http.Get("https://127.0.0.1:2999/liveclientdata/activeplayer")
+      func main() {
+             resp, err := http.Get("https://127.0.0.1:2999/liveclientdata/activeplayer")
 
-    if err != nil {
-    }
+      if err != nil {
+        // handle error
+       }
+      defer resp.Body.Close()
 
+                           
+                
 }
