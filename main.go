cc10f600a3bf4abbdc7121064daf6e054e531a79
Léopold <leopold.pieto@gmail.com>
2021-04-14 18:23:33 +0200

    Last version

--- main.go
+++ main.go
@@ -7,15 +7,12 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-
 )
 
 const https = "https://euw1.api.riotgames.com"
-
 const APIkey = "?api_key=RGAPI-549361bd-d625-4629-b7c2-dc246de49620" //valable 24h
 const accountId = "D8lq_rQ9lXxYdl867SpZMRo6UqH7fAJ3hmaQSJ8sbqlPrV8"
 const puuid = "Y6dqJmXGwwagdqXz27gSpv5Mf_J5xZ2owmIcpek6LLvJnSJ0nPwCY984LzDWIomm1omlLIShODcenw"
-
 const tagLine = "EUW"
 const gameName = "mouton211"
 
@@ -35,48 +32,31 @@ func apiprofil(urlprofil string) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	
 	apiData:=playerData{}
         err=json.Unmarshal(body,&apiData)
 
-        if err != nil {
+	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(apiData)
-
+        fmt.Println(apiData)
 }
 
-/*func apiclient(urlclient string) {
-
-	response, err := http.Get(urlclient)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(response, string(body))
-
-}*/
 func main() {
     for{
 	urlprofil := https + "/lol/summoner/v4/summoners/by-name/" + gameName + APIkey
-	//urlclient := "https://127.0.0.1:2999/liveclientdata/allgamedata"
 
 	apiprofil(urlprofil)
 	fmt.Println()
-	//apiclient(urlclient)
-
-	fmt.Println("###########################################################")
-	fmt.Println()
-
-	fmt.Println("Nom du joueur:", gameName, "  Niveaux", "  Serveur:", tagLine)
-	fmt.Println()
-
-	fmt.Println("###########################################################")
 	
+	fmt.Println("###########################################################")
 	fmt.Println()
+
+	fmt.Println("Nom du joueur:", "  Niveau:", "  Serveur:", tagLine)
+	fmt.Println()
+
+	fmt.Println("###########################################################")
+
 	time.Sleep(100*time.Millisecond)
     }
 }
