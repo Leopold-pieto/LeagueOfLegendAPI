35f78c054a173790b003f40e668fe5a3e7e318a1
Léopold <leopold.pieto@gmail.com>
2021-04-15 15:10:51 +0200

    Last Version

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 )
 
 const https = "https://euw1.api.riotgames.com"
-const APIkey = "?api_key=RGAPI-15c29020-b2c8-4423-9ae1-7b18e959af2a" //valable 24h
+const APIkey = "?api_key=RGAPI-6c5f3688-5786-4b0b-b6cd-b319482d0e96" //valable 24h
 const accountId = "D8lq_rQ9lXxYdl867SpZMRo6UqH7fAJ3hmaQSJ8sbqlPrV8"
 const puuid = "Y6dqJmXGwwagdqXz27gSpv5Mf_J5xZ2owmIcpek6LLvJnSJ0nPwCY984LzDWIomm1omlLIShODcenw"
 const tagLine = "EUW"
@@ -20,10 +20,9 @@ const gameName = "mouton211"
 type playerData struct {
 	SummonerLevel int16
 	Name          string
-
 }
 
-func apiprofil(urlprofil string) playerData {
+func apiProfil(urlprofil string) playerData {
 	response, err := http.Get(urlprofil)
 	if err != nil {
 		log.Fatal(err)
@@ -33,24 +32,40 @@ func apiprofil(urlprofil string) playerData {
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println(string(body))
 
 	apiData := playerData{}
 	err = json.Unmarshal(body, &apiData)
 	if err != nil {
 		log.Fatal(err)
 	}
-  
-	fmt.Println(apiData)
+
 	return apiData
+}
+
+func apiStat(urlstat string) {
+	response, err := http.Get(urlstat)
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(response, string(body))
 }
 
 func main() {
 	for {
 		urlprofil := https + "/lol/summoner/v4/summoners/by-name/" + gameName + APIkey
-		apiprofil(urlprofil)
+		playerInfo := apiProfil(urlprofil)
+
+		urlstat := "https://127.0.0.1:2999/liveclientdata/allgamedata"
+		apiStat(urlstat)
 
 		fmt.Println("###########################################################")
-		fmt.Println("Nom du joueur:", "  Niveau:", summonerLevel, "  Serveur:", tagLine)
+		fmt.Println("Nom du joueur:", playerInfo.Name, "  Niveau:", playerInfo.SummonerLevel, "  Serveur:", tagLine)
 		fmt.Println("###########################################################")
 		fmt.Println()
 		time.Sleep(100 * time.Millisecond)
