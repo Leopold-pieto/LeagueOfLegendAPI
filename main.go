6c12da7b0da026b131710fd1dbffe412d78024c8
Léopold <leopold.pieto@gmail.com>
2021-04-15 10:00:23 +0200

    try to return valuse from apiData

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-        "encoding/json"
+	"encoding/json"
 	"fmt"
-	"time"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
 
 const https = "https://euw1.api.riotgames.com"
-const APIkey = "?api_key=RGAPI-15c29020-b2c8-4423-9ae1-7b18e959af2a"//valable 24h
+const APIkey = "?api_key=RGAPI-15c29020-b2c8-4423-9ae1-7b18e959af2a" //valable 24h
 const accountId = "D8lq_rQ9lXxYdl867SpZMRo6UqH7fAJ3hmaQSJ8sbqlPrV8"
 const puuid = "Y6dqJmXGwwagdqXz27gSpv5Mf_J5xZ2owmIcpek6LLvJnSJ0nPwCY984LzDWIomm1omlLIShODcenw"
 const tagLine = "EUW"
@@ -18,11 +18,11 @@ const gameName = "mouton211"
 
 // 20 requests every 1 seconds(s) & 100 requests every 2 minutes(s)
 type playerData struct {
-       SummonerLevel int16
-       Name string
+	SummonerLevel int16
+	Name          string
 }
 
-func apiprofil(urlprofil string) {
+func apiprofil(urlprofil string) playerData {
 	response, err := http.Get(urlprofil)
 	if err != nil {
 		log.Fatal(err)
@@ -32,31 +32,35 @@ func apiprofil(urlprofil string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	
-	apiData:=playerData{}
-        err=json.Unmarshal(body,&apiData)
+
+	apiData := playerData{}
+	err = json.Unmarshal(body, &apiData)
 
 	if err != nil {
 		log.Fatal(err)
 	}
-        fmt.Println(apiData)
+	fmt.Println(apiData)
+	return apiData
 }
 
 func main() {
-    for{
-	urlprofil := https + "/lol/summoner/v4/summoners/by-name/" + gameName + APIkey
+	for {
+		//level:=SummonerLevel
+		//name:=Name
 
-	apiprofil(urlprofil)
-	fmt.Println()
-	
-	fmt.Println("###########################################################")
-	fmt.Println()
+		urlprofil := https + "/lol/summoner/v4/summoners/by-name/" + gameName + APIkey
 
-	fmt.Println("Nom du joueur:", "  Niveau:", "  Serveur:", tagLine)
-	fmt.Println()
+		apiprofil(urlprofil)
+		fmt.Println()
 
-	fmt.Println("###########################################################")
+		fmt.Println("###########################################################")
+		fmt.Println()
 
-	time.Sleep(100*time.Millisecond)
-    }
+		fmt.Println("Nom du joueur:", "  Niveau:", summonerLevel, "  Serveur:", tagLine)
+		fmt.Println()
+
+		fmt.Println("###########################################################")
+
+		time.Sleep(100 * time.Millisecond)
+	}
 }
